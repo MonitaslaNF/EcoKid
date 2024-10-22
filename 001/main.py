e64d366e2c6edf54ec67586eb3b71d315b52e05a
MonitaslaNF <monitasla.mnf@gmail.com>
2024-10-21 22:51:13 -0300

    Add files via upload

---
+++ 001/main.py
@@ -0,0 +1,35 @@
+
+
+from flask import Flask, render_template, redirect, request
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ECOKIDS'
+
+
+
+@app.route('/')
+def home():
+        return render_template('login.html')
+
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    
+    nome = request.form.get('nome')
+    senha = request.form.get('senha')    
+    print(nome)
+    print(senha)
+    return redirect('/')
+
+
+
+
+
+
+if __name__ in '__main__':
+      app.run(debug=True)
+      
+
+
